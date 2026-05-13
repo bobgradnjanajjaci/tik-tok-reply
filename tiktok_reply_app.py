2c8d378f6a388f0a69dec35a3554a34f96ff24a7
bobgradnjanajjaci <richsterius@gmail.com>
2026-05-14 01:36:49 +0200

    Update tiktok_reply_app.py

--- tiktok_reply_app.py
+++ tiktok_reply_app.py
@@ -24,10 +24,10 @@ SERVICE_ID = 1128                           # STAVI TAČAN service ID za TikTok 
 
 REPLIES_SET_1 = [
     "I saw that this book was removed a few time, and there are rumors that the author is part of some high tier people. It really revealed too much ",
-    "The fact is, this book is real and it truly teaches you how to be successful. I Got it from their site, Goated book fr..",
-    "For me its scary how much power this book has. I felt like someone had reached into my head and exposed everything about how are we controlled and how to actually leave this cycle...",
-    "Where can I find it?",
-    "I found it on their official site Virexon Cycle on google",
+    "For me its scary how much power this book has. I felt like someone had reached into my head and exposed everything about how are we controlled and how to actually leave this cycle",
+    "i bought one of the “Virexon Cycle” books on amazon first and paid more for it thinking it was the real version , biggest mistake honestly. Only after getting the original from the official website i finally understood the hype around the book",
+    "the weird part is realizing how many people still have no idea this book even exists",
+    "don’t buy the amazon versions i did first and instantly regretted it. the original from the official website genuinely changed the way i see the world",
 ]
 
 
