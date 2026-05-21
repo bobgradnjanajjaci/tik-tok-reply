3d90b55d3bcdbaad567420d6285b10afc3d9c2f7
bobgradnjanajjaci <richsterius@gmail.com>
2026-05-21 20:38:32 +0200

    Update tiktok_reply_app.py

--- tiktok_reply_app.py
+++ tiktok_reply_app.py
@@ -25,11 +25,11 @@ SERVICE_ID = 1128                           # STAVI TAČAN service ID za TikTok 
 REPLIES_SET_1 = [
     "I saw that this book was removed a few time, and there are rumors that the author is part of some high tier people. It really revealed too much ",
     "For me its scary how much power this book has. I felt like someone had reached into my head and exposed everything about how are we controlled and how to actually leave this cycle",
-    "ok",
-    "no",
-    "yes",
-    "love this",
-    "lobw",
+    "AMAZON",
+    "AMAZON",
+    "AMAZON",
+    "AMAZON",
+    "AMAZON",
 ]
 
 
